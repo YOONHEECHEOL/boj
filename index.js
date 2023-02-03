ee5345a64b615c7144370badcf7a814d9e6b6f2d
YOONHEECHEOL <luk2903201@naver.com>
2023-02-03 17:24:45 +0900

    euclid update

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 const solution = (l, m) => {
 
-    const euclid = (a, b) => {
-        // a가 큰 수, b가 작은 수
-        let max = 0;
+    // const euclid = (a, b) => {
+    //     // a가 큰 수, b가 작은 수
+    //     let max = 0;
     
-        let r = a % b;
-        if(r === 0) return b;
-        if(r !== 0) {
-            return euclid(b, r);
-        }
-    }
+    //     let r = a % b;
+    //     if(r === 0) return b;
+    //     if(r !== 0) {
+    //         return euclid(b, r);
+    //     }
+    // }
 
-    let answer = euclid(l, m);
-    return answer;
+    // let answer = euclid(l, m);
+    // return answer;
+
+    let max = l;
+    let min = m;
+    console.log(max % min === 0)
+    while(max % min !== 0) {
+        let r = max % min;
+        if(r !== 0) {
+            max = m;
+            min = r;
+        }
+    } 
+
+    // min = 최대공약수
+    let lcm = (l * m) / min;
+
+    return [lcm, min];
 }
   
 
 console.log(solution(12, 8));
-
