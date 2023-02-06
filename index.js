2ee55d3c5a3e8f302ae9122661f1b6243f44c6de
YOONHEECHEOL <luk2903201@naver.com>
2023-02-06 17:47:09 +0900

    최빈값 풀이

--- index.js
+++ index.js
@@ -2,38 +2,32 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-const solution = (l, m) => {
+const solution = (arr) => {
 
-    // const euclid = (a, b) => {
-    //     // a가 큰 수, b가 작은 수
-    //     let max = 0;
-    
-    //     let r = a % b;
-    //     if(r === 0) return b;
-    //     if(r !== 0) {
-    //         return euclid(b, r);
-    //     }
-    // }
+    let a = arr.reduce((acc, curr) => {
 
-    // let answer = euclid(l, m);
-    // return answer;
-
-    let max = l;
-    let min = m;
-    console.log(max % min === 0)
-    while(max % min !== 0) {
-        let r = max % min;
-        if(r !== 0) {
-            max = m;
-            min = r;
+        if(acc.chked.includes(curr)) {
+            acc.val = acc.val.map(i => {
+                if(i.v === curr) {
+                    i.cnt += 1;
+                    return i;
+                } else return i;
+            })
+            return acc;
+        } else {
+            acc.chked.push(curr);
+            acc.val.push({v: curr, cnt: 1});
+            return acc;
         }
-    } 
+        
+    }, {val: [], chked: []})   
+    
+    a = a.val.sort((l, m) => m.cnt - l.cnt);
 
-    // min = 최대공약수
-    let lcm = (l * m) / min;
+    if(a[0].cnt === a[1].cnt) return -1
+    else return a[0].cnt;
 
-    return [lcm, min];
 }
   
 
-console.log(solution(12, 8));
+console.log(solution([1, 1, 2, 2]));
